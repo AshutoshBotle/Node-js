edb0554a5514d655c56c02e583482135006cce8c
AshutoshBotle <Ashutosh@123>
2025-01-15 13:26:58 +0000

    chnagedd to new

--- mynodeapp/index.js
+++ mynodeapp/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
-var app = express();//Respond with "hello world" for requests that hit our root "/"
+var app = express(); // Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('changed nor');
-});//listen to port 3000 by default
-app.listen(process.env.PORT || 3000);
- 
+  res.send('changed nor');
+}); 
+
+// Listen on all interfaces (0.0.0.0) and default port 3000
+app.listen(process.env.PORT || 3000, '0.0.0.0', function () {
+  console.log('Server is running on port 3000');
+});
+
 module.exports = app;
